1592714f7a05ef378900ba6d03eb4e3b43bb912f
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-09-22 23:34:12 -0600

    Configuración del mainpage

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import "./Mainpage.css";
+import menu1 from "./img/Menu.jpeg"
+import menu2 from "./img/menu2.jpg"
 
 function Mainpage (){
     return(
         <div>
             <Header/>
+            <main className="menuu">
+                <img src={menu1} alt="menupizzas"/>
+                <img src={menu2} alt="menupizzas2"/>
+                <br></br>
+                <br></br>
+                <br></br>
+            </main>
             <Footer/>
         </div>
     )
